dd5b5cf9f8eed632a20ea68927cc9b7dc5027e5d
ericczekner <czeknere@gmail.com>
2025-01-22 16:49:19 -0500

    Disable @typescript-eslint/no-unused-expressions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,7 @@ const eslintConfig = [
 			"@typescript-eslint/no-explicit-any": "off",
 			"@typescript-eslint/no-unused-vars": "off",
 			"prefer-const": "off",
+			"@typescript-eslint/no-unused-expressions": "off",
 		},
 	}),
 ];
